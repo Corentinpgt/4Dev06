4220dba0630bb1d70f3a45740b9740418614fcd2
Corentinpgt <corentin.pouget.19@gmail.com>
2024-03-05 09:55:50 +0100

    t

---
+++ _Projet_1_Social-mediia-dashboard-Start/src/App.jsx
@@ -0,0 +1,26 @@
+export default function App() {
+
+  
+  return (
+    <>
+      <div className=" flex flex-col bg-card_bg w-60 text-text1 items-center">
+        <figure className=" flex gap-2 ">
+            <img className="" src="../../assets/icon-facebook.svg" alt="" />
+            <figcaption className=" ">
+                @nathanf
+            </figcaption>
+        </figure>
+        <section className="">
+          <p className=""><span className="text-text2">1987</span> <br /> Followers
+          </p>
+        </section>
+        <figure>
+          <img src="../../assets/icon-up.svg" alt="" />
+          <figcaption>
+            12 Today
+          </figcaption>
+        </figure>
+      </div>
+      </>
+  )
+}
---
+++ _Projet_1_Social-mediia-dashboard-Start/src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card(social) {
+    return (
+      <>
+      <div className="">
+        <figure className="">
+            <img className="" src="../../assets/icon-facebook.svg" alt="" />
+            <figcaption className="">
+                @nathanf
+            </figcaption>
+        </figure>
+      </div>
+      </>
+    )
+  }
