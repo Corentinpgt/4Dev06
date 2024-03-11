550ebd092f35095a4f5fb0ea143130657ee30299
Corentinpgt <148058562+Corentinpgt@users.noreply.github.com>
2024-03-11 11:57:50 +0100

    Update App.jsx

--- TD_TW_adv/Start/src/App.jsx
+++ TD_TW_adv/Start/src/App.jsx
@@ -1,5 +1,67 @@
-import Ex from "./exercices/exercice1/Ex1_1";
+import { cva } from "class-variance-authority";
+
+// Base definition
+const base = "text-white text-center mt-2"
+
+const button = cva(base, {
+  variants: {
+    intent: {
+      free: [
+        "bg-green-700",
+        "p-2",
+        "rounded-md",
+        "font-bold",
+        "hover:bg-green-800",
+      ],
+      sub: [
+        "bg-yellow-600",
+        "p-2",
+        "rounded-md",
+        "font-bold",
+        "hover:bg-yellow-700",
+      ],
+      rent: [
+        "bg-red-500",
+        "p-2",
+        "rounded-md",
+        "font-bold",
+        "hover:bg-red-700",
+      ],
+      simple: [
+
+      ],
+    },
+    border : {
+      none: [""],
+      solid: ["border-2", "border-white"]
+    }
+    // size: {
+    //   big: ["text-sm", "py-1", "px-2"],
+    //   medium: ["text-base", "py-2", "px-4"],
+    // },
+  },
+  // compoundVariants: [{ intent: "primary", size: "medium", class: "uppercase" }],
+  // defaultVariants: {
+  //   intent: "primary",
+  //   size: "medium",
+  // },
+});
+
+// function Button({ intent, size, className, ...rest }) {
+//   return <button {...rest} className={button({ intent, size, className })} />;
+// }
+
+function Button({ intent, border, className, ...rest }) {
+  return <button {...rest} className={button({ intent, border, className })} />;
+}
 
 export default function App() {
-  return <Ex />;
+  return(
+    <div className="grid h-screen place-content-center bg-black">
+      <Button intent="rent" border="none">Explore Rentals</Button>
+      <Button intent="sub" border="none">Explore Subscription</Button>
+      <Button intent="free" border="solid">Explore Free</Button>
+      <Button intent="simple" border="none">Explore Free</Button>
+    </div>
+  )
 }
